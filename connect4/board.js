ba031716948f23f538d8e11ec539a51b128612a5
dylans <dylan@dojotoolkit.org>
2012-12-01 12:11:40 -0700

    add initial files for game

---
+++ connect4/board.js
@@ -0,0 +1,13 @@
+//load the AMD modules we need
+define([], function(){
+  return {
+    init: function(){
+      // draw canvas or use background image
+
+    },
+
+    reset: function(){
+      // reset the board when the game is over
+    }
+  };
+});
